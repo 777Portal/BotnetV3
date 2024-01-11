9a430f1770a4c85baa7f1089357ed7aa43473d1b
777Portal <throwawayact69xd@gmail.com>
2024-01-11 18:51:30 -0500

    Updated client to send all events to the main server

--- botClient.js
+++ botClient.js
@@ -7,32 +7,30 @@ var lastX,
 	lastZ
 
 const config = {
-	host: 'constantiam.net',
+	host: '2b2t.org',
 	port: 25565,
 	interval: 1000 * 10 // cooldown between joining server too prevent joining too quickly
 }
 
 // socket stuff
-
 socket.on('connect', () => {
 	console.log('Connected to socket successfully');
 	
 	socket.emit("reqEmail", {type: "bot"}); // gets email from main websocket.
 });
 
-var email;
+var email; // used later on for reconnecting when disconnected.
 
-socket.on('email', (data) => {
-	// Got email from main websocket.
-
-	email = data // declaring var used when reconnectingss
+socket.on('email', (data) => { // got email from mainHost
+	email = data
 	  console.log(`Recieved email: ${email}`)
 	  spawnAndBind(email)
 });
 
+var ready = false // checking if we can send data via websocket.
+
 // mineflayer stuff
 function spawnAndBind(email){
-	console.log("")
 
 	var bot = mineflayer.createBot({
 		username: email,
@@ -41,16 +39,19 @@ function spawnAndBind(email){
 		port: config.port,
 		version: false
 	})
-	
+
 	console.log(`Using ${email} as email.`)
 	
-	// bot.on(event, function (...args){
-	// 	console.log(event, args);
-	// });
-
+	socket.on('bot', (username) => {
+		username = username.username
+		if (username == bot.username) ready = true
+	})
+	
 	bot.on('end', function(reason){
 		console.log(email)
-		socket.emit("endBot", {username: bot.username, reason: reason})
+		setTimeout(() => {
+			spawnAndBind(email)
+		}, 1000);
 	})
 
 	bot.on('resourcePack', function () {
@@ -67,47 +68,41 @@ function spawnAndBind(email){
 
 		socket.on('disconnect', () => {
 			console.log('Disconnected from the (website) websocket.');
-			if (bot) {
-				bot.removeAllListeners(); // Remove all event listeners attached to the bot
-				bot.end("noMoreWebsiteSocket"); // Cleanly ends the Minecraft bot connection
-				bot = null; // Set the bot variable to null to discard the old bot instance
-				console.log("Stopped bot")
-			}
+			
+			if (!bot) return // checks if bot still exists.
+
+			console.log("Stopped bot")
+			bot.removeAllListeners(); // Eemoves all event listeners attached to the bot
+			bot.end("noMoreWebsiteSocket"); // Cleanly ends the Minecraft bot connection
+			bot = null; // Set the bot variable to null to discard the old bot instance
 		});
 
 		// ie Webveiwer thingy loads the page
 		socket.on('reqPosUpdate', () => {
+			
 			console.log("Received request to send position to server...");
 		
 			if (bot && bot.entity && bot.entity.position && typeof bot.entity.position.x !== 'undefined') {
-				const { x, y, z } = bot.entity.position;
+			  const { x, y, z } = bot.entity.position;
 		
-				console.log(`Sending position update: ${x}, ${y}, ${z}`);
+			  console.log(`Sending position update: ${x}, ${y}, ${z}`);
 		
-				setTimeout(() => {
-					socket.emit("posUpdate", { username: bot.username, x, y, z });
-				}, 1000 * 10); // Delayed emission after 1 second to hope the other thinggy is loaded
+			  setTimeout(() => {
+				socket.emit("posUpdate", { username: bot.username, x, y, z });
+			  }, 1000 * 10); // Delayed emission after 1 second to hope the other thinggy is loaded
 			} else {
-				console.log("Bot position data is not available or undefined.");
-				socket.emit("posUpdate", { username: "error", x:"Cords", y:"Not", z:"found" });
+			  console.log("Bot position data is not available or undefined.");
+			  socket.emit("posUpdate", { x:"Cords", y:"Not", z:"Found" });
 			}
-		});
-		
-
-		// mineflayerViewer(bot, { port: `300${bots.length}`, firstPerson: false })	
-		bot.on('message', function(raw) {
-			raw = raw.toString();
-	
-			socket.emit("chatMsg", {bot: bot.username, message: raw}) // sending message
-			console.log(raw)
-		})
-		
+		});		
 	})
 
 	bot.on('kicked', function(reason) {
-		socket.emit("chatMsg", {bot: bot.username, message: reason})
 		console.log("Kicked for ", reason);
-		setTimeout(() => {
+		socket.emit("chatMsg", {bot: bot.username, message: reason})
+		
+		setTimeout(() => 
+		{
 			spawnAndBind(email)
 		}, 1000);
 	});
@@ -115,5 +110,25 @@ function spawnAndBind(email){
 	bot.on('spawn', () => console.log(bot.username))
 
 	bot.on('error', (err) => console.log(err))
+	
+	patchEmitter(bot)
 }
 
+function patchEmitter(emitter) {
+  var oldEmit = emitter.emit;
+  
+  emitter.emit = function() {
+	oldEmit.apply(emitter, arguments);		
+	if (ready == false) return
+
+	const emitArgs = arguments;
+		
+	eventName = (emitArgs && emitArgs[0]) ?? "UndefinedEventName"
+	eventArguments = (emitArgs && emitArgs[1]) ?? false
+
+	if (eventName == "newListener" || eventName == "removeListener" || eventName == "connect" || eventName == "physicTick" || eventName == "physicsTick") return // baddddd throws ERROR
+	if (!eventArguments) return socket.emit(eventName) // no args 
+
+	socket.emit(eventName, eventArguments); // yes args.
+  }
+}
